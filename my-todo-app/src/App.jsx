6f497ba9637685bb35d60a8a54c74a87862ea2cc
Srijal Prasad <srijalprasad@gmail.com>
2024-08-23 21:16:18 +0530

    Commiting

---
+++ my-todo-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux'
+import './App.css'
+import Todo from './components/Todo'
+
+function App() {
+
+  return (
+ <Provider>
+   <Todo/>
+ </Provider>
+  )
+}
+
+export default App
---
+++ my-todo-app/src/components/Todo.jsx
@@ -0,0 +1,10 @@
+
+function Todo() {
+  return (
+    <div>
+      <h1>To-do App</h1>
+    </div>
+  )
+}
+
+export default Todo
